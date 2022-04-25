990bb3d1039651123f6de1659b8bb21b10f7e217
Juan Morales <launchx10261@innovaccion.mx>
2022-04-25 15:33:45 -0500

    Agregando con exito el Jest y ejecutando mi index.js desde npm start

---
+++ index.js
@@ -0,0 +1,7 @@
+import MissionCommander from './app/missionCommander.js';
+const juan = new MissionCommander("Juan")
+const william = new MissionCommander("Will")
+const owen = new MissionCommander("El Nene")
+console.log(juan.name)
+console.log(william.name)
+console.log(owen.name)
